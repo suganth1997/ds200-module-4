13b087476da0820fbeb2b39f64ef670e8a356b11
suganth1997 <suganth.ilangovan@gmail.com>
2019-10-11 20:04:47 +0530

    Updated Paths

--- box.py
+++ box.py
@@ -3,10 +3,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from random import shuffle
 
-data = pd.read_csv('~/Downloads/Export201718.csv')
+data = pd.read_csv('./Export201718.csv')
 value = data.iloc[:, -1][:-1] #Last Vlaue was a outlier
 plt.boxplot(value)
 plt.yscale('log')
 plt.ylabel('Export Value in USD')
 plt.grid()
-plt.savefig('/home/stuti/Desktop/Box_Plot.png', dpi=300, format='png', bbox_inches='tight')
+plt.savefig('Box_Plot.png', dpi=300, format='png', bbox_inches='tight')
