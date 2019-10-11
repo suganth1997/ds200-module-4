cb223b90f437f469d32656b72de1894c6c1fb1f8
suganth1997 <suganth.ilangovan@gmail.com>
2019-10-11 20:05:12 +0530

    Updated Paths

--- scatter.py
+++ scatter.py
@@ -2,8 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from random import shuffle
 
-data = pd.read_csv('~/Downloads/Export201718.csv')
+data = pd.read_csv('./Export201718.csv')
 plt.scatter(data.iloc[:, -2], data.iloc[:, -1])
 plt.xlabel('EXPORT QUANTITY')
 plt.ylabel('EXPORT VALUE in USD')
-plt.savefig('/home/stuti/Desktop/Scatter_Plot.png', dpi=300, format='png', bbox_inches='tight')
+plt.savefig('Scatter_Plot.png', dpi=300, format='png', bbox_inches='tight')
